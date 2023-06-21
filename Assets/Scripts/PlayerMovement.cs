be01681bf347196413a6e3fd0869ccc7c7a4342f
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2023-06-21 16:02:59 -0400

    Added steps sound
    Aniadi efecto de sonido a los pasos

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,15 @@ public class PlayerMovement : MonoBehaviour
 {
     private const string MovementVertical = "Vertical";
     private const string MovementHorizontal = "Horizontal";
+    public AudioSource clip;
 
     [SerializeField] private float _speed = 15f;
 
     private Rigidbody2D _rigidBody;
     private Vector2 _moveInput;
     private Animator _animator;
+    private bool Hactivo;
+    private bool Vactivo;
 
     void Start()
     {
@@ -40,6 +43,27 @@ public class PlayerMovement : MonoBehaviour
 
         _animator.SetFloat(MovementHorizontal, moveX);
         _animator.SetFloat(MovementVertical, moveY);
+
+        if (Input.GetButtonDown(MovementHorizontal)){
+            Hactivo = true;
+            clip.Play();
+        }
+        if (Input.GetButtonDown(MovementVertical)){
+            Vactivo = true;
+            clip.Play();
+        }
+        if (Input.GetButtonUp(MovementHorizontal)){
+            if (Vactivo == false){
+                clip.Pause();
+            }            
+            Hactivo = false;
+        }
+        if (Input.GetButtonUp(MovementVertical)){
+            if (Hactivo == false){
+                clip.Pause();
+            }
+            Vactivo = false;
+        }
     }
 
     private void FixedUpdate()
