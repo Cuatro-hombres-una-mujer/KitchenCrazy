0c2332db4a0f342363e1a50a27eb60e533116e43
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2023-06-25 00:13:26 -0400

    feat: Added option change quality and fix script "opciones" method to change volume

--- Assets/Scripts/Opciones.cs
+++ Assets/Scripts/Opciones.cs
@@ -3,26 +3,75 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class Opciones : MonoBehaviour
 {   
+    public Slider slider;
+    public float sliderValue;
+    public Image imagenMute;
     public AudioSource clip;
+    public Toggle toggle;
+    public TMP_Dropdown dropdown;
+    public int calidad;
 
-    [SerializeField] private AudioMixer audioMixer;
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("volumenAudio", 0.5f);
+        AudioListener.volume = slider.value;
+        RevisarSiEstoyMute();
 
-    public void PantallaCompleta(bool pantallaCompleta)
+        if(Screen.fullScreen)
+        {
+            toggle.isOn = true;
+        }
+        else
+        {
+            toggle.isOn = false;
+        }
+
+        calidad = PlayerPrefs.GetInt("numeroDeCalidad", 3);
+        dropdown.value = calidad;
+        AjustarCalidad();
+    }
+
+    public void ChangeSlider(float valor)
+    {
+        sliderValue = valor;
+        PlayerPrefs.SetFloat("volumenAudio", sliderValue);
+        AudioListener.volume = slider.value;
+        RevisarSiEstoyMute();
+    }
+
+    public void RevisarSiEstoyMute()
+    {
+        if(sliderValue == 0)
+        {
+            imagenMute.enabled = true;
+        }
+        else
+        {
+            imagenMute.enabled = false;
+        }
+    }
+
+    public void ActivarPantallaCompleta(bool pantallaCompleta)
     {
         Screen.fullScreen = pantallaCompleta;
-    }
-    public void CambiarVolumen(float Volumen)
-    {
-        audioMixer.SetFloat("Volumen", Volumen);
     }
 
     public void ButtonExit()
     {
         clip.Play();
         SceneManager.LoadScene(0);
+    }
+
+    public void AjustarCalidad()
+    {
+        QualitySettings.SetQualityLevel(dropdown.value);
+        PlayerPrefs.SetInt("numeroDeCalidad", dropdown.value);
+        calidad = dropdown.value;
     }
 
 }
