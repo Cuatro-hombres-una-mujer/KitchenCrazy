801c3aea24c45714818f76ba66491a53389e11f2
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2023-06-30 15:07:00 -0400

    feat; added button start cooking

--- Assets/Scripts/Cronometro.cs
+++ Assets/Scripts/Cronometro.cs
@@ -19,7 +19,6 @@ public class Cronometro : MonoBehaviour
         tiempoSegundos = Mathf.FloorToInt(tiempo % 60);
         tiempoDecimasSegundo = Mathf.FloorToInt((tiempo % 1) * 100);
 
-
         textoCrono.text = string.Format("{0:00}:{1:00}:{2:00}", tiempoMinutos, tiempoSegundos, tiempoDecimasSegundo);
    }
     void Update()
