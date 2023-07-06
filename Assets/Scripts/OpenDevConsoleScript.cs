841f68923377a2629de0ab98b5b209c790e883b3
Benjamín Miranda <supremobenjamin.cia@gmail.com>
2023-07-06 00:12:19 -0400

    updated

---
+++ Assets/Scripts/OpenDevConsoleScript.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class OpenDevConsoleScript : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.developerConsoleVisible = true;
+        }
+        
+    }
+    
+}
